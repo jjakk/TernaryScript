39eb95dd65afa81b554369e6bd315e08bd534615
John Khachian <jaksawesome10@gmail.com>
2022-06-19 11:49:44 -0400

    print new line operation

--- src/parser/operations/print.c
+++ src/parser/operations/print.c
@@ -1,4 +1,16 @@
 
 void printDefault(Line line){
-    printf("%s\n", line.secondOperand);
+    printf("%s", line.secondOperand);
+}
+
+void printNewLine(){
+    printf("\n");
+}
+
+int printVariable(Line line){
+    char *variableName = line.secondOperand;
+    Variable *variable = getVariable(variableName);
+    if(variable == pRootVariable) return 1;
+    printf("%s", variable->value);
+    return 0;
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,24 +1,26 @@
-#include "./operations/print.c"
+
 #include "./operations/createVariable.c"
 #include "./operations/setVariable.c"
-#include "./operations/printVariable.c"
+#include "./operations/print.c"
 
 int parse(Line line){
     char *operation = line.firstOperand;
     if(operation[0] == '/' && operation[1] == '/'){
         return 0;
     }
-    else if(strcmp(operation, "PRINT") == 0){
-        printDefault(line);
-    } else if(strcmp(operation, "PRINT_VARIABLE") == 0){
-        if(printVariable(line) == 1){
-            printf("\nERROR: Invalid variable `%s` on Line #%i\n", line.secondOperand, line.number);
-            return 1;
-        }
-    } else if(strcmp(operation, "CREATE_VARIABLE") == 0){
+    else if(strcmp(operation, "CREATE_VARIABLE") == 0){
         createVariable(line);
     } else if(strcmp(operation, "SET_VARIABLE") == 0){
         if(setVariable(line) == 1){
+            printf("\nERROR: Invalid variable `%s` on Line #%i\n", line.secondOperand, line.number);
+            return 1;
+        }
+    } else if(strcmp(operation, "PRINT") == 0){
+        printDefault(line);
+    } else if(strcmp(operation, "PRINT_NEW_LINE") == 0){
+        printNewLine();
+    } else if(strcmp(operation, "PRINT_VARIABLE") == 0){
+        if(printVariable(line) == 1){
             printf("\nERROR: Invalid variable `%s` on Line #%i\n", line.secondOperand, line.number);
             return 1;
         }
